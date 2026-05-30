018bca03ab53ca7479c271bfdb3c9e1623ec2db6
rbxStardev <miguelscb0310@gmail.com>
2026-05-29 22:20:49 -0300

    Environment package setup

---
+++ wrappedPrograms/environment.nix
@@ -0,0 +1,68 @@
+{
+  lib,
+  inputs,
+  self,
+  ...
+}: {
+  perSystem = {
+    pkgs,
+    self',
+    ...
+  }: {
+    packages.terminal =
+      (inputs.wrappers.wrapperModules.alacritty.apply {
+        inherit pkgs;
+        imports = [self.wrappersModules.alacritty];
+        shell = lib.getExe self'.packages.environment;
+      }).wrapper;
+
+    packages.environment = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = self'.packages.zsh;
+      runtimeInputs = [
+        # nix
+        pkgs.nil
+        pkgs.nixd
+        pkgs.statix
+        pkgs.alejandra
+        pkgs.manix
+        pkgs.nix-inspect
+        self'.packages.nh
+
+        # other
+        pkgs.file
+        pkgs.unzip
+        pkgs.zip
+        pkgs.p7zip
+        pkgs.wget
+        pkgs.killall
+        pkgs.fzf
+        pkgs.htop
+        pkgs.btop
+        pkgs.eza
+        pkgs.fd
+        pkgs.ripgrep
+        pkgs.fastfetch
+        pkgs.tree-sitter
+        pkgs.imagemagick
+        pkgs.ffmpeg-full
+        pkgs.yt-dlp
+        pkgs.lazygit
+
+        # wrapper
+        self'.packages.git
+        self'.packages.nix-check-bin
+      ];
+      env = {
+        EDITOR = lib.getExe self'.packages.helix;
+      };
+    };
+
+    packages.nix-check-bin = pkgs.writeShellApplication {
+      name = "nix-check-bin";
+      text = ''
+        $EDITOR "$(nix build "$1" --no-link --print-out-paths)/bin"
+      '';
+    };
+  };
+}

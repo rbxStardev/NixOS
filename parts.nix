71d8dc808d7825930c6be03a1296c83e2f004c09
rbxStardev <miguelscb0310@gmail.com>
2026-05-29 22:32:24 -0300

    Added merging support for wrappersModules

--- parts.nix
+++ parts.nix
@@ -7,7 +7,11 @@
   options = {
     flake = inputs.flake-parts.lib.mkSubmoduleOptions {
       wrappersModules = inputs.nixpkgs.lib.mkOption {
+        type = inputs.nixpkgs.lib.types.lazyAttrsOf inputs.nixpkgs.lib.types.deferredModule;
         default = {};
+        apply = inputs.nixpkgs.lib.mapAttrs (_: v: {
+          imports = [v];
+        });
       };
     };
   };

c0df15d888999536db781f900cf4c66eb503594d
rbxStardev <miguelscb0310@gmail.com>
2026-06-03 17:19:39 -0300

    oops

--- modules/features/firefox.nix
+++ modules/features/firefox.nix
@@ -1,10 +1,12 @@
 {
   flake.nixosModules.firefox = {pkgs, ...}: {
-    programs.firefox.enable = true;
+    programs.firefox = {
+      enable = true;
 
-    preferences = {
-      "widget.use-xdg-desktop-portal.file-picker" = 1;
-      "widget.use-xdg-desktop-portal.mime-handler" = 1;
+      preferences = {
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "widget.use-xdg-desktop-portal.mime-handler" = 1;
+      };
     };
   };
 }

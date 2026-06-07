e56c4c39d42a5f94584447ca2432dbbdb695fdf9
rbxStardev <miguelscb0310@gmail.com>
2026-06-07 19:37:44 -0300

    noctalia tweaks

--- modules/features/assets.nix
+++ modules/features/assets.nix
@@ -2,19 +2,19 @@
 {lib, ...}: {
   options.assets = {
     wallpaperDir = lib.mkOption {
-      default = "~/Assets/wallpapers";
+      default = "/home/star/Assets/Wallpapers";
     };
     avatarPicture = lib.mkOption {
-      default = "~/Assets/face.png";
+      default = "/home/star/Assets/face.png";
     };
     notifCritical = lib.mkOption {
-      default = "~/Assets/sounds/critical.mp3";
+      default = "/home/star/Assets/Sounds/critical.mp3";
     };
     notifNormal = lib.mkOption {
-      default = "~/Assets/sounds/normal.mp3.mp3";
+      default = "/home/star/Assets/Sounds/normal.mp3.mp3";
     };
     notifLow = lib.mkOption {
-      default = "~/Assets/sounds/low.mp3.mp3";
+      default = "/home/star/Assets/Sounds/low.mp3.mp3";
     };
   };
 }

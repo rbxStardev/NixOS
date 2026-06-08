7208c198c3638e2b3700b3f8b4d3fad3bf021306
rbxStardev <miguelscb0310@gmail.com>
2026-06-08 15:54:33 -0300

    temporarily added some things on gaming

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -1,9 +1,32 @@
 {
-  flake.nixosModules.gaming = {pkgs, ...}: {
+  flake.nixosModules.gaming = {pkgs, ...}: let
+    jsab = pkgs.makeDesktopItem {
+      name = "just-shapes-and-beats";
+      desktopName = "Just Shapes and Beats";
+      comment = "Play this game on Steam";
+      exec = "steam steam://rungameid/10725085396061913088";
+      icon = "/home/star/Games/Just Shapes and Beats v1.6.50/icon.png";
+      categories = ["Game"];
+      terminal = false;
+    };
+
+    legoBatmanLotDK = pkgs.makeDesktopItem {
+      name = "lego-batman";
+      desktopName = "LEGO Batman: Legacy of the Dark Knight";
+      comment = "Play this game on Steam";
+      exec = "steam steam://rungameid/9700019291125972992";
+      icon = "/home/star/Games/LEGO Batman - Legacy of the Dark Knight/icon.png";
+      categories = ["Game"];
+      terminal = false;
+    };
+  in {
     hardware.graphics.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      hydralauncher
+    environment.systemPackages = [
+      pkgs.hydralauncher
+
+      jsab
+      legoBatmanLotDK
     ];
 
     programs = {

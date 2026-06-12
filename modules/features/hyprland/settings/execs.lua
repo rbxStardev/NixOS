332822c1e2db3d66b1f0ec72ca72b1982e1d46ac
rbxStardev <miguelscb0310@gmail.com>
2026-06-12 14:31:57 -0300

    add dbus update to hyprland exec

--- modules/features/hyprland/settings/execs.lua
+++ modules/features/hyprland/settings/execs.lua
@@ -3,6 +3,7 @@
 
 -- Event listener: Executes processes once upon Hyprland startup.
 hl.on("hyprland.start", function(...)
+	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 	hl.exec_cmd("mpd-discord-rpc")
